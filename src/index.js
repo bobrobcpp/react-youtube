0aa5d3128280ca9e7eec225a99bb3da16067e4c0
bobrobcpp <king.roob@gmail.com>
2017-09-21 12:17:59 +0100

    Loads video-list-items with onClick event working

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
 import YTSearch from 'youtube-api-search';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 // import VideoListItem from './components/video_list_item';
 const API_KEY = "AIzaSyBtPn-benyFiWWsRkpGT7P4Qd4ztpF-Pd8";
 
@@ -10,10 +11,16 @@ const API_KEY = "AIzaSyBtPn-benyFiWWsRkpGT7P4Qd4ztpF-Pd8";
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { videos: [] };
+    this.state = {
+      videos: [],
+      selectedVideo: null
+       };
 
-    YTSearch({key: API_KEY, term: 'surfboards'}, (videos)  =>{
-     this.setState({ videos });
+    YTSearch({key: API_KEY, term: 'offshore survey'}, (videos)  =>{
+     this.setState({
+      videos: videos,
+      selectedVideo: videos[0]
+       });
     });
 
   }
@@ -21,8 +28,11 @@ class App extends Component {
   render() {
     return (
       <div>
-      <SearchBar />
-      <VideoList videos={this.state.videos} />
+      {/* <SearchBar */}
+      <VideoDetail video={this.state.selectedVideo} />
+      <VideoList
+      onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+      videos={this.state.videos} />
       </div>
     );
 
